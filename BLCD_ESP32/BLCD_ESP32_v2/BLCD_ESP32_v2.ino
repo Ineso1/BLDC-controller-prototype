d7fc71a57cceb9a489f575b0bcdb549c835638b7
Ineso1 <inesgamos@gmail.com>
2023-04-03 03:21:51 -0500

    Update BLCD_ESP32_v2.ino

--- BLCD_ESP32/BLCD_ESP32_v2/BLCD_ESP32_v2.ino
+++ BLCD_ESP32/BLCD_ESP32_v2/BLCD_ESP32_v2.ino
@@ -17,26 +17,29 @@ Description: A BLDC controller xd
   - GPIO 26: PWM de la fase A
   - GPIO 25: PWM de la fase B
   - GPIO 33: PWM de la fase C
+  - GPIO 32: PWM de la fase AA
+  - GPIO 35: PWM de la fase BB
+  - GPIO 34: PWM de la fase CC
   - GPIO 13: Pin del potenciómetro
  */
 
-const int pot_pin = 13;              // Potentiometer pin
+const int pot_pin = 22;              // Potentiometer pin
 const float pwm_min = 0.0;           // Minimum PWM duty cycle
 const float pwm_max = 255.0;         // Maximum PWM duty cycle
-const float pwm_frequency = 1;  // PWM frequency in Hz
+const float pwm_frequency = 1000;  // PWM frequency in Hz
 const int num_poles = 8;              // Number of poles in the motor
 const int num_comm_steps = 6;         // Number of commutation steps
-const int hall_a_pin = 12;            // Hall sensor A pin
-const int hall_b_pin = 14;            // Hall sensor B pin
-const int hall_c_pin = 27;            // Hall sensor C pin
-const int pwm_pin_a = 26;             // PWM pin for phase A
-const int pwm_pin_b = 25;             // PWM pin for phase B
-const int pwm_pin_c = 33;             // PWM pin for phase C
+const int hall_a_pin = 21;            // Hall sensor A pin
+const int hall_b_pin = 19;            // Hall sensor B pin
+const int hall_c_pin = 18;            // Hall sensor C pin
+const int pwm_pin_c = 26;             // PWM pin for phase A
+const int pwm_pin_b = 32;             // PWM pin for phase B
+const int pwm_pin_a = 34;             // PWM pin for phase C
 
 //Fasess prueba
-const int pwm_pin_aa = 32;             // PWM pin for phase A
-const int pwm_pin_bb = 35;             // PWM pin for phase B
-const int pwm_pin_cc = 34;             // PWM pin for phase C
+const int pwm_pin_cc = 25;             // PWM pin for phase A
+const int pwm_pin_bb = 33;             // PWM pin for phase B
+const int pwm_pin_aa = 35;             // PWM pin for phase C
 
 
 /*
@@ -59,9 +62,9 @@ const int commutation_table[num_comm_steps][3] = {
 };
 */
 
-/*
+
 //Fases prueba
-const int commutation_table[num_comm_steps][6] = {
+const int commutation_table_decode[num_comm_steps][6] = {
   {0, 0, 1, 1, 0, 0},
   {0, 1, 0, 1, 0, 0},
   {0, 1, 0, 0, 0, 1},
@@ -69,8 +72,6 @@ const int commutation_table[num_comm_steps][6] = {
   {1, 0, 0, 0, 1, 0},
   {0, 0, 1, 0, 1, 0}
 };
-*/
-
 
 //Fases prueba nuestro gate driver
 const int commutation_table[num_comm_steps][6] = {
@@ -139,23 +140,23 @@ void loop() {
   int hall_state = (hall_a_val << 2) | (hall_b_val << 1) | hall_c_val;
   int commutation_step = 0;
   switch (hall_state) {
-    case 0b001:
+    case 0b110:
+      commutation_step = 5;
+      break;
+    case 0b010:
       commutation_step = 0;
       break;
     case 0b011:
       commutation_step = 1;
       break;
-    case 0b010:
+    case 0b001:
       commutation_step = 2;
       break;
-    case 0b110:
+    case 0b101:
       commutation_step = 3;
       break;
     case 0b100:
       commutation_step = 4;
-      break;
-    case 0b101:
-      commutation_step = 5;
       break;
   }
 
@@ -200,6 +201,10 @@ void loop() {
   digitalWrite(pwm_pin_cc, phase_cc_duty);
   /*............................*/
 
+
+
+  Serial.print(hall_state, BIN);
+  Serial.print("\t---");
   Serial.print(phase_a_duty);
   Serial.print(" - ");
   Serial.print(phase_b_duty);
@@ -214,4 +219,47 @@ void loop() {
   Serial.print(" - ");
   Serial.println(phase_cc_duty);
   delay(10);
+
+
+/*
+  Serial.print("\t---");
+  Serial.print(hall_a_val);
+  Serial.print(" - ");
+  Serial.print(hall_b_val);
+  Serial.print(" - ");
+  Serial.print(hall_c_val);
+  Serial.print("\t---");
+  
+  Serial.print(hall_state, BIN);
+  Serial.print("\t---");
+  Serial.print(commutation_table_decode[commutation_step][0]);
+  Serial.print(" - ");
+  Serial.print(commutation_table_decode[commutation_step][1]);
+  Serial.print(" - ");
+  Serial.print(commutation_table_decode[commutation_step][2]);
+
+  //........Fases prueba
+  Serial.print(" ---- ");
+  Serial.print(commutation_table_decode[commutation_step][3]);
+  Serial.print(" - ");
+  Serial.print(commutation_table_decode[commutation_step][4]);
+  Serial.print(" - ");
+  Serial.print(commutation_table_decode[commutation_step][5]);
+
+  Serial.print("\t|||\t");
+  Serial.print(phase_a_duty);
+  Serial.print(" - ");
+  Serial.print(phase_b_duty);
+  Serial.print(" - ");
+  Serial.print(phase_c_duty);
+
+  //........Fases prueba
+  Serial.print(" ---- ");
+  Serial.print(phase_aa_duty);
+  Serial.print(" - ");
+  Serial.print(phase_bb_duty);
+  Serial.print(" - ");
+  Serial.println(phase_cc_duty);
+  delay(10);
+  */
 }
