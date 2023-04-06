d73a22fec7e3ae928196b439dd67715a090e1795
Ineso1 <inesgamos@gmail.com>
2023-04-06 09:55:55 -0500

    conmutacion bien

--- BLCD_ESP32/BLDC_ESP32_vArduino/BLDC_Arduino/BLDC_Arduino.ino
+++ BLCD_ESP32/BLDC_ESP32_vArduino/BLDC_Arduino/BLDC_Arduino.ino
@@ -28,7 +28,7 @@ const float pwm_min = 0.0;           // Minimum PWM duty cycle
 const float pwm_max = 255.0;         // Maximum PWM duty cycle
 const float pwm_frequency = 1000;  // PWM frequency in Hz
 const int num_poles = 8;              // Number of poles in the motor
-const int num_comm_steps = 6;         // Number of commutation steps
+const int num_comm_steps = 7;         // Number of commutation steps
 const int hall_a_pin = 8;            // Hall sensor A pin
 const int hall_b_pin = 9;            // Hall sensor B pin
 const int hall_c_pin = 10;            // Hall sensor C pin
@@ -44,7 +44,7 @@ const int pwm_pin_aa = 12;             // PWM pin for phase C
 
 int AH, AL, BH, BL, CH, CL;
 float phase_a_duty, phase_b_duty, phase_c_duty;
-
+float pwm_duty = 0;
 /*
   Tabla de conmutación
 
@@ -54,41 +54,54 @@ float phase_a_duty, phase_b_duty, phase_c_duty;
 */
 
 //Fases prueba nuestro gate driver
-const int commutation_table[num_comm_steps][6] = {
+/*const int commutation_table[num_comm_steps][7] = {
   {0, 1, 2, 1, 1, 0},
   {1, 0, 2, 1, 1, 0},
   {1, 2, 0, 1, 0, 1},
   {0, 2, 1, 1, 0, 1},
   {2, 0, 1, 0, 1, 1},
-  {2, 1, 0, 0, 1, 1}
+  {2, 1, 0, 0, 1, 1},
+  {0, 0, 0, 0, 0, 0}
   
 };
 
-const int commutation_table_reverseC[num_comm_steps][6] = {
+const int commutation_table_reverseC[num_comm_steps][7] = {
   {0, 2, 1, 1, 0, 1},
   {1, 2, 0, 1, 0, 1},
   {1, 0, 2, 1, 1, 0},
   {0, 1, 2, 1, 1, 0},
   {2, 1, 0, 0, 1, 1},
-  {2, 0, 1, 0, 1, 1}
+  {2, 0, 1, 0, 1, 1},
+  {0, 0, 0, 0, 0, 0}
   
 };
 
-const int commutation_table_a[num_comm_steps][6] = {
+const int commutation_table_a[num_comm_steps][7] = {
   {0, 2, 1, 1, 0, 1},
   {2, 0, 1, 0, 1, 1},
   {2, 1, 0, 0, 1, 1},
   {0, 1, 2, 1, 1, 0},
   {1, 0, 2, 1, 1, 0},
-  {1, 2, 0, 1, 0, 1}
+  {1, 2, 0, 1, 0, 1},
+  {0, 0, 0, 1, 1, 1}
   
 };
+*/
 
+const int commutation_table[num_comm_steps][7] = {
+  {0, 0, 2, 1, 0, 1},
+  {2, 0, 0, 1, 0, 1},
+  {2, 0, 0, 1, 1, 0},
+  {0, 2, 0, 1, 1, 0},
+  {0, 2, 0, 0, 1, 1},
+  {0, 0, 2, 0, 1, 1},
+  {0, 0, 0, 0, 0, 0}
+};
 
 void setup() {
   Serial.begin(9600);
   
-  pinMode(A0, INPUT);
+  //pinMode(A0, INPUT);
   pinMode(hall_a_pin, INPUT);
   pinMode(hall_b_pin, INPUT);
   pinMode(hall_c_pin, INPUT);
@@ -109,11 +122,11 @@ void setup() {
 void loop() {
   int hall_a_val = !digitalRead(hall_a_pin);
   int hall_b_val = !digitalRead(hall_b_pin);
-  //int hall_c_val = !digitalRead(hall_c_pin);
+  int hall_c_val = !digitalRead(hall_c_pin);
 
 
 
-  
+  /*
   float hall_c_val_raw = analogRead(hall_c_pin);
   int hall_c_val = 0;
   if(hall_c_val_raw > 1000){
@@ -122,6 +135,7 @@ void loop() {
   else{
     hall_c_val = 1;  
   }
+*/
 
   /*
   float hall_c_val_raw = analogRead(hall_c_pin);
@@ -137,23 +151,26 @@ void loop() {
   int commutation_step = 0;
   switch (hall_state) {
     case 0b101:
-      commutation_step = 0;
-      break;
-    case 0b001:
       commutation_step = 1;
       break;
-    case 0b011:
+    case 0b001:
       commutation_step = 2;
       break;
-    case 0b010:
+    case 0b011:
       commutation_step = 3;
       break;
-    case 0b110:
+    case 0b010:
       commutation_step = 4;
       break;
-    case 0b100:
+    case 0b110:
       commutation_step = 5;
       break;
+    case 0b100:
+      commutation_step = 0;
+      break;
+    default:
+      commutation_step = 6;
+    break;
   }
 
 /*
@@ -164,8 +181,8 @@ void loop() {
   Rango de convercion 0 - 255 (pwm_min - pwm_max) 
 */
 
-  int pot_val = analogRead(A0)*10/2;
-  float pwm_duty = map(pot_val, 0, 4095, pwm_min, pwm_max);
+  int pot_val = analogRead(A0);
+  pwm_duty = map(pot_val, 160, 900, pwm_min, pwm_max);
 
   AH = commutation_table[commutation_step][0];
   AL = commutation_table[commutation_step][3];
@@ -174,6 +191,7 @@ void loop() {
   CH = commutation_table[commutation_step][2];
   CL = commutation_table[commutation_step][5];
 
+/*
   if(AH == 2){
       phase_a_duty =  pwm_duty;
       analogWrite(pwm_pin_a, phase_a_duty);
@@ -200,16 +218,26 @@ void loop() {
       phase_c_duty = CH;
       digitalWrite(pwm_pin_c, CH);
     }
+    */
 
+  int phase_a_duty = AH * pwm_duty /2;
+  int phase_b_duty = BH * pwm_duty /2;
+  int phase_c_duty = CH * pwm_duty /2;
   int phase_aa_duty = AL;
   int phase_bb_duty = BL;
   int phase_cc_duty = CL;
+  
+  analogWrite(pwm_pin_a, phase_a_duty);
+  analogWrite(pwm_pin_b, phase_b_duty);
+  analogWrite(pwm_pin_c, phase_c_duty);
   
   digitalWrite(pwm_pin_aa, phase_aa_duty);
   digitalWrite(pwm_pin_bb, phase_bb_duty);
   digitalWrite(pwm_pin_cc, phase_cc_duty);
 
 /*
+  Serial.print(pot_val);
+  Serial.print("\t--");
   Serial.print(pwm_duty);
   Serial.print("\t--");
   Serial.print(hall_c_val);
@@ -220,9 +248,9 @@ void loop() {
   Serial.print(" - ");
   Serial.print(hall_c_val);
   Serial.print("\t---");
-*/  
+    
   //Serial.println(hall_state, BIN);
-/*
+
   Serial.print("\t|||\t");
   Serial.print(phase_a_duty);
   Serial.print(" - ");
@@ -238,5 +266,6 @@ void loop() {
   Serial.print(" - ");
   Serial.println(phase_cc_duty);
   //delay(10);
+  
 */
 }
