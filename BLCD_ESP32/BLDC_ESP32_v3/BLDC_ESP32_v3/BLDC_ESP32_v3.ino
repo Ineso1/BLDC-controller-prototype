96805b2d4f5d6e4eb011746e060db64b8456e8f6
Ineso1 <inesgamos@gmail.com>
2023-04-11 13:56:27 -0500

    Codigo oficial BLDC_ESP32_v3
    Aumento de frecuencia

--- BLCD_ESP32/BLDC_ESP32_v3/BLDC_ESP32_v3/BLDC_ESP32_v3.ino
+++ BLCD_ESP32/BLDC_ESP32_v3/BLDC_ESP32_v3/BLDC_ESP32_v3.ino
@@ -1,12 +1,11 @@
-#include <analogWrite.h>
-
+//Codigo oficial 
 /**********************************************
 
 Project Name: BLDC controller
 Author: Elyos team assfasfas
 Date: 2022-03-03
 Microcontroller: ESP 32
-Description: A BLDC controller version 3
+Description: A BLDC controller version 3 
 
 *************************************************/
  
@@ -15,20 +14,20 @@ Description: A BLDC controller version 3
   Pinout:
   - GPIO 21: Hall A
   - GPIO 19: Hall B
-  - GPIO 18: Hall C
-  - GPIO 34: PWM de la fase A
+  - GPIO 13: Hall C
+  - GPIO 27: PWM de la fase A
   - GPIO 32: PWM de la fase B
   - GPIO 26: PWM de la fase C
-  - GPIO 34: PWM de la fase AA
-  - GPIO 32: PWM de la fase BB
+  - GPIO 12: PWM de la fase AA
+  - GPIO 33: PWM de la fase BB
   - GPIO 26: PWM de la fase CC
-  - GPIO 22: Pin del potenciómetro
+  - GPIO 4: Pin del potenciómetro
  */
 
 const int pot_pin = 4;              // Potentiometer pin
 const float pwm_min = 0.0;           // Minimum PWM duty cycle
 const float pwm_max = 255.0;         // Maximum PWM duty cycle
-const float pwm_frequency = 1000;  // PWM frequency in Hz
+const float pwm_frequency = 20000;  // PWM frequency in Hz
 const int num_poles = 8;              // Number of poles in the motor
 const int num_comm_steps = 6;         // Number of commutation steps
 const int hall_a_pin = 21;            // Hall sensor A pin
@@ -38,7 +37,7 @@ const int pwm_pin_c = 26;             // PWM pin for phase A
 const int pwm_pin_b = 32;             // PWM pin for phase B
 const int pwm_pin_a = 27;             // PWM pin for phase C
 
-//Fasess prueba
+//Fases prueba
 const int pwm_pin_cc = 25;             // PWM pin for phase A
 const int pwm_pin_bb = 33;             // PWM pin for phase B
 const int pwm_pin_aa = 12;             // PWM pin for phase C
@@ -56,29 +55,19 @@ float phase_a_duty, phase_b_duty, phase_c_duty;
 */
 
 //Fases prueba nuestro gate driver
-const int commutation_table_a[num_comm_steps][6] = {
-  {0, 1, 2, 1, 1, 0},
-  {2, 1, 0, 0, 1, 1},
-  {2, 0, 1, 0, 1, 1},
-  {0, 2, 1, 1, 0, 1},
-  {1, 2, 0, 1, 0, 1},
-  {1, 0, 2, 1, 1, 0}
-  
-};
-
-const int commutation_table[num_comm_steps][6] = {
-  {0, 2, 1, 1, 0, 1},
-  {1, 2, 0, 1, 0, 1},
-  {1, 0, 2, 1, 1, 0},
-  {0, 1, 2, 1, 1, 0},
-  {2, 1, 0, 0, 1, 1},
-  {2, 0, 1, 0, 1, 1}
-  
+const int commutation_table[num_comm_steps][7] = {
+  {0, 0, 2, 1, 0, 1},
+  {2, 0, 0, 1, 0, 1},
+  {2, 0, 0, 1, 1, 0},
+  {0, 2, 0, 1, 1, 0},
+  {0, 2, 0, 0, 1, 1},
+  {0, 0, 2, 0, 1, 1},
+  {0, 0, 0, 0, 0, 0}
 };
 
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   
   pinMode(hall_a_pin, INPUT);
   pinMode(hall_b_pin, INPUT);
@@ -107,38 +96,33 @@ void setup() {
 void loop() {
   int hall_a_val = !digitalRead(hall_a_pin);
   int hall_b_val = !digitalRead(hall_b_pin);
-  //int hall_c_val = !digitalRead(hall_c_pin);
-  
-  float hall_c_val_raw = analogRead(hall_c_pin);
-  int hall_c_val = 0;
-  if(hall_c_val_raw > 1000){
-    hall_c_val = 0;  
-  }
-  else{
-    hall_c_val = 1;  
-  }
+  int hall_c_val = !digitalRead(hall_c_pin);
+
 
   int hall_state = (hall_a_val << 2) | (hall_b_val << 1) | hall_c_val;
   int commutation_step = 0;
   switch (hall_state) {
-    case 0b110:
-      commutation_step = 0;
-      break;
-    case 0b010:
+    case 0b101:
       commutation_step = 1;
       break;
-    case 0b011:
+    case 0b001:
       commutation_step = 2;
       break;
-    case 0b001:
+    case 0b011:
       commutation_step = 3;
       break;
-    case 0b101:
+    case 0b010:
       commutation_step = 4;
       break;
-    case 0b100:
+    case 0b110:
       commutation_step = 5;
       break;
+    case 0b100:
+      commutation_step = 0;
+      break;
+    default:
+      commutation_step = 6;
+    break;
   }
 
 /*
@@ -159,76 +143,21 @@ void loop() {
   CH = commutation_table[commutation_step][2];
   CL = commutation_table[commutation_step][5];
 
-  if(AH == 2){
-      phase_a_duty =  pwm_duty;
-      ledcWrite(0, phase_a_duty);
-      //analogWrite(pwm_pin_a, phase_a_duty);
-    }
-  else{
-      phase_a_duty = AH * pwm_max;
-      ledcWrite(0, phase_a_duty);
-      //digitalWrite(pwm_pin_a, AH);
-     }
-     
-  if(BH == 2){
-      phase_b_duty = pwm_duty;
-      ledcWrite(1, phase_b_duty);
-      //analogWrite(pwm_pin_b, phase_b_duty);
-    }
-  else{
-      phase_b_duty = BH * pwm_max;
-      ledcWrite(1, phase_b_duty);
-      //digitalWrite(pwm_pin_b, BH);
-    }
-    
-  if(CH == 2){
-      phase_c_duty = pwm_duty;
-      ledcWrite(2, phase_c_duty);
-      //analogWrite(pwm_pin_c, phase_c_duty); 
-    }
-  else{
-      phase_c_duty = CH * pwm_max;
-      ledcWrite(2, phase_c_duty);
-      //digitalWrite(pwm_pin_c, CH);
-    }
-
+  int phase_a_duty = AH * pwm_duty /2;
+  int phase_b_duty = BH * pwm_duty /2;
+  int phase_c_duty = CH * pwm_duty /2;
   int phase_aa_duty = AL;
   int phase_bb_duty = BL;
   int phase_cc_duty = CL;
+
+
+  ledcWrite(0, phase_a_duty);
+  ledcWrite(1, phase_b_duty);
+  ledcWrite(2, phase_c_duty);
 
   digitalWrite(pwm_pin_aa, phase_aa_duty);
   digitalWrite(pwm_pin_bb, phase_bb_duty);
   digitalWrite(pwm_pin_cc, phase_cc_duty);
 
-/*
-  Serial.print(pwm_duty);
-  Serial.print("\t--");
-  Serial.print(hall_c_val);
-  Serial.print("\t---");
-  Serial.print(hall_a_val);
-  Serial.print(" - ");
-  Serial.print(hall_b_val);
-  Serial.print(" - ");
-  Serial.print(hall_c_val);
-  Serial.print("\t---");
-  
-  Serial.print(hall_state, BIN);
-
-  Serial.print("\t|||\t");
-  Serial.print(phase_a_duty);
-  Serial.print(" - ");
-  Serial.print(phase_b_duty);
-  Serial.print(" - ");
-  Serial.print(phase_c_duty);
-
-  //........Fases prueba
-  Serial.print(" ---- ");
-  Serial.print(phase_aa_duty);
-  Serial.print(" - ");
-  Serial.print(phase_bb_duty);
-  Serial.print(" - ");
-  Serial.println(phase_cc_duty);
-  delay(10);
-  */
 
 }
